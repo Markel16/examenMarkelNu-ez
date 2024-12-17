433fe01d571e5a1fe55ad666056e408ee09dff5d
Markel16 <markel_nunez@euneiz.com>
2024-12-17 09:25:15 +0100

    introduzco un for y un while y aparte unos texxtos para dar feedback al usuario

--- MarkelNuñez/Program.cs
+++ MarkelNuñez/Program.cs
@@ -3,4 +3,14 @@
     int cantidadDeNumeros = 10;
     int[] numeros = new int[cantidadDeNumeros];
     Console.WriteLine($"Introduce {cantidadDeNumeros} una cantidad de numeros enteros");
+
+    for (int i = 0; i < cantidadDeNumeros; i++)
+    {
+        Console.WriteLine($"Coloca un numero {i + 1}:");
+
+        while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.Write("Solo valen numeros, prueba de nuevo:");
+        }
+    }
 }
