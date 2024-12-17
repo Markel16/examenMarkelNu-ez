eff8db05f61f1ff9701b689560e1b033cc0247d7
Markel16 <markel_nunez@euneiz.com>
2024-12-17 09:26:42 +0100

    para imprimir la lsita del usuario

--- MarkelNuñez/Program.cs
+++ MarkelNuñez/Program.cs
@@ -13,4 +13,16 @@
             Console.Write("Solo valen numeros, prueba de nuevo:");
         }
     }
+    Console.WriteLine("\n  esta es tu lista de numeros que has puesto: ");
+    for (int i = 0; i < cantidadDeNumeros; i++)
+    {
+        if (i == cantidadDeNumeros - 1)
+        {
+            Console.Write(numeros[i]);
+
+        }
+        else
+            Console.Write(numeros[i] + " ");
+    }
+    Console.WriteLine();
 }
